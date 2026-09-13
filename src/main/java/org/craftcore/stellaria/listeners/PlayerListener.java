d2cea8e9ae293901a05fd0d43c14665bebaa697b
shu <shu28940@gmail.com>
2026-09-13 16:28:51 +0900

    tpa移植

--- src/main/java/org/craftcore/stellaria/listeners/PlayerListener.java
+++ src/main/java/org/craftcore/stellaria/listeners/PlayerListener.java
@@ -3,11 +3,17 @@ package org.craftcore.stellaria.listeners;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.craftcore.stellaria.commands.tpa.TpaCore;
 
 public class PlayerListener implements Listener {
     
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         // Handle player join event
+    }
+    @EventHandler
+    public void onPlayerLeave(PlayerQuitEvent event){
+        TpaCore.resetPlayerTeleportRequests(event.getPlayer());
     }
 }
